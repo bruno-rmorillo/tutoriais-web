e48b6f5a2fca0c41be3397ca657fedb6fc998943
BRUNO ROBERTO MORILLO <37968675+bruno-rmorillo@users.noreply.github.com>
2020-08-10 00:17:44 -0300

    Novo tutorial

---
+++ PHP - Pegar data atual/index.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+echo date("Y-m-d H:i:s");
+
+
+/*
+* Y - Ano por extenso.      1998.
+* y - Ano encurtado.        98.
+* m - Mês em número.        12.
+* d - Dia por extenso.      11.
+* H - Hora no formato 24.   23.
+* h - Hora no formato 12.   11.
+* i - Minutos em número.    46.
+* s - Segundos em número.   59.
+*/
